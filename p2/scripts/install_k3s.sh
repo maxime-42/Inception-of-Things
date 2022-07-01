4667ee5d1d29839a4f7f481cb207b5f88f66e401
ggjulio <juligonz@student.42.fr>
2022-07-01 17:02:20 +0200

    p2 shared folder not working properly...

---
+++ p2/scripts/install_k3s.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+RESET="\e[0m"
+LIGHT_RED="\e[91m"
+LIGHT_GREEN="\e[92m"
+
+set -eu
+
+logging(){
+	local type=$1; shift
+	printf "${RESET}[%b] $0 : %b\n" "$type" "$*"
+}
+
+log_info(){
+	logging "${LIGHT_GREEN}info${RESET}" "$@"
+}
+
+log_error(){
+	logging "${LIGHT_RED}error${RESET}" "$@" >&2
+	exit 1
+}
+
+if [ -z "$NODE_IP" ]; then
+   log_error "No variable NODE_IP specified. Please specify it."
+fi
+
+log_info "Setting up mirrors and dnf update..."
+sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+# dnf -y update
+
+log_info "Installing server k3S with ${NODE_IP} as node ip..."
+# curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--cluster-init --node-ip=${NODE_IP} --write-kubeconfig-mode 644" sh -
+log_info "k3S server node installed."
+
+log_info "Setting up kubectl completion..."
+# /usr/local/bin/kubectl completion bash >> /etc/bash_completion.d/kubectl
