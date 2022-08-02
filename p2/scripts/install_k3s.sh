7eda44c706113056558c4cc990616b400c06c279
ggjulio <juligonz@student.42.fr>
2022-08-02 04:15:22 +0200

    p2: issue #12 fixed by disabling the firewall before running the k3s install script

--- p2/scripts/install_k3s.sh
+++ p2/scripts/install_k3s.sh
@@ -4,7 +4,7 @@ RESET="\e[0m"
 LIGHT_RED="\e[91m"
 LIGHT_GREEN="\e[92m"
 
-set -eux
+set -eu
 
 logging(){
 	local type=$1; shift
@@ -24,12 +24,12 @@ if [ -z "$NODE_IP" ]; then
    log_error "No variable NODE_IP specified. Please specify it."
 fi
 
-log_info "Installing server k3S with ${NODE_IP} as node ip..."
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--cluster-init --node-ip=${NODE_IP} --flannel-iface=eth1 --write-kubeconfig-mode 644" sh -
-log_info "k3S server node installed."
-
 log_info "Disabling firewall..."
 sudo systemctl disable firewalld --now # https://rancher.com/docs/k3s/latest/en/advanced/#additional-preparation-for-red-hat-centos-enterprise-linux
+
+log_info "Installing server k3S with ${NODE_IP} as node ip..."
+curl -sfL https://get.k3s.io | sh -s - server --node-ip="${NODE_IP}" --flannel-iface=eth1 --write-kubeconfig-mode 644
+log_info "k3S server node installed."
 
 log_info "Setting up kubectl completion..."
 /usr/local/bin/kubectl completion bash >> /etc/bash_completion.d/kubectl
