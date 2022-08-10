e99c09fae716de5009834968ff5373028d98d52a
ggjulio <juligonz@student.42.fr>
2022-08-10 18:39:47 +0200

    maybe

--- bonus/scripts/backup_gitlab.sh
+++ bonus/scripts/backup_gitlab.sh
@@ -4,26 +4,22 @@
 # backup-utility source: https://gitlab.com/gitlab-org/build/CNG/-/blob/master/gitlab-toolbox/scripts/bin/backup-utility
 # https://www.youtube.com/watch?v=G-KZzn1f-i8&ab_channel=LinuxHelp
 
-set -eux
+set -eu
 
 cd $(dirname $0)
 
-gitlab_backups_path=/srv/gitlab/tmp/backups/
-
+gitlab_backups_path='/srv/gitlab/tmp/backups/'
 pod_name_toolbox="$(kubectl get pod -n gitlab | grep toolbox | awk '{print $1;}')"
 
+rm -rf ../config/gitlab/
+mkdir -p ../config/gitlab/backup
 
 kubectl -n gitlab exec -i "$pod_name_toolbox" -- bash -c \
 	"rm -rf $gitlab_backups_path \
 	&& gitlab-rake gitlab:backup:create"
 kubectl cp gitlab/"$pod_name_toolbox":"$gitlab_backups_path" ../config/gitlab/backup && \
-# kubectl cp gitlab/"$pod_name_toolbox":/srv/gitlab/tmp/backups/repositories ../config/gitlab/backup/repositories
 kubectl -n gitlab exec -i "$pod_name_toolbox" -- bash -c \
 	"rm -rf $gitlab_backups_path \
 	&& mkdir -p $gitlab_backups_path"
 
-
-# kubectl -n gitlab exec "$pod_name_toolbox" -- gitlab-rake gitlab:backup:create
-# cp ...
-# kubectl -n gitlab exec "$pod_name_toolbox" -- rm -rf "$gitlab_backups_path"
-# kubectl -n gitlab exec "$pod_name_toolbox" -- mkdir -p "$gitlab_backups_path"
+kubectl -n gitlab get secrets gitlab-rails-secret -o yaml > ../config/gitlab/gitlab-rails-secret.yaml
--- bonus/scripts/restore_gitlab.sh
+++ bonus/scripts/restore_gitlab.sh
@@ -7,21 +7,43 @@
 
 set -eux
 
-
 cd $(dirname $0)
 
-gitlab_backups_local_path="../config/gitlab/backup/"
-gitlab_backups_path="/srv/gitlab/tmp/backups/"
-newest_backup_file="$(ls -Art $gitlab_backups_local_path | tail -n 1)"
-pod_name_toolbox="$(kubectl get pod -n gitlab | grep toolbox | awk '{print $1;}')"
+helm_release='gitlab'
 
-kubectl -n gitlab exec -i "$pod_name_toolbox" -- mkdir -p $gitlab_backups_path
-kubectl cp $gitlab_backups_local_path/"$newest_backup_file" gitlab/"$pod_name_toolbox":"$gitlab_backups_path"
-kubectl -n gitlab exec -i "$pod_name_toolbox" -- gitlab-rake gitlab:backup:restore force=yes
+do_backup(){
+	local pod_name_toolbox="$(kubectl get pod -n gitlab | grep toolbox | awk '{print $1;}')"
+	local backups_local_path="../config/gitlab/backup/"
+	local backups_pod_path="/srv/gitlab/tmp/backups/"
+	local newest_backup_file="$(ls -Art $backups_local_path | tail -n 1)"
 
-# kubectl -n gitlab exec -i "$pod_name_toolbox" -- bash -c \
-# 	"gitlab-ctl stop unicron \
-# 	&& gitlab-ctl stop sidekiq \
-# 	&& gitlab-ctl stop\
-# 	&& gitlab-rake gitlab:backup:restore BACKUP=${newest_backup_file} force=yes
-# 	&& gitlab-ctl start"
+	kubectl -n gitlab exec -i "$pod_name_toolbox" -- mkdir -p $backups_pod_path
+	kubectl cp $backups_local_path/"$newest_backup_file" gitlab/"$pod_name_toolbox":"$backups_pod_path"
+	kubectl -n gitlab exec -i "$pod_name_toolbox" -- gitlab-rake gitlab:backup:restore force=yes
+}
+
+replace_secret(){
+	if kubectl -n gitlab get secrets | grep -q gitlab-rails-secret ; then
+		kubectl -n gitlab delete secret gitlab-rails-secret
+		echo 'rail secret deleted'
+		kubectl -n gitlab apply -f ../config/gitlab/gitlab-rails-secret.yaml
+	else
+		echo 'default secret not found ! should wait ?'
+		exit 42
+	fi
+}
+
+delete_gitlab_pods(){
+	kubectl -n gitlab delete pods -lapp=sidekiq,release="$helm_release"
+	kubectl -n gitlab delete pods -lapp=webservice,release="$helm_release"
+	kubectl -n gitlab delete pods -lapp=toolbox,release="$helm_release"
+}
+
+main()
+{
+	replace_secret
+	delete_gitlab_pods
+	do_backup
+}
+
+main "$@"
